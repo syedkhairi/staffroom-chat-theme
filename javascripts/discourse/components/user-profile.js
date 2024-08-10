f6f37e24d9a5e6dbd6daae93384fb09e0d28a2d7
Syed <syedkhairi@yahoo.com>
2024-08-10 01:31:10 +0100

    layout

--- javascripts/discourse/components/user-profile.js
+++ javascripts/discourse/components/user-profile.js
@@ -3,5 +3,14 @@ import { inject as service } from "@ember/service";
 
 export default class SidebarWelcome extends Component {
   @service router;
+  @service composer;
+  @service siteSettings;
+  @service site;
   @service currentUser;
+
+  get isTopRoute() {
+    const { currentRoute } = this.router;
+    const topMenuRoutes = this.siteSettings.top_menu.split("|").filter(Boolean);
+    return topMenuRoutes.includes(currentRoute.localName);
+  }
 }
