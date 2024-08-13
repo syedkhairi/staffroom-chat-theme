d6dafe3413add656cf3f20502ab0812207c5ace8
Syed <syedkhairi@yahoo.com>
2024-08-13 15:49:49 +0100

    fake input create

--- javascripts/discourse/components/fake-input-create.js
+++ javascripts/discourse/components/fake-input-create.js
@@ -2,11 +2,14 @@ import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import Composer from "discourse/models/composer";
 import { inject as service } from "@ember/service";
+import { getURLWithCDN } from "discourse-common/lib/get-url";
+import { tracked } from "@glimmer/tracking";
 
 export default class FakeInputCreate extends Component {
   @service composer;
   @service router;
   @service currentUser;
+  @tracked flairUrl = null;
 
   get category() {
     return this.router.currentRoute?.attributes?.category;
@@ -27,6 +30,18 @@ export default class FakeInputCreate extends Component {
         categoryId: this.category?.id,
         tags: this.tag?.id,
       });
+    }
+  }
+
+  @action
+  async fetchBgUrl() {
+   try {
+      const response = await fetch(`/u/${this.currentUser.username}.json`);
+      const data = await response.json(); // assuming this is json
+      const userFlairUrl = data.user.flair_url;
+      this.flairUrl = getURLWithCDN(userFlairUrl);
+    } catch (error) {
+      console.error("Failed:", error);
     }
   }
 }
